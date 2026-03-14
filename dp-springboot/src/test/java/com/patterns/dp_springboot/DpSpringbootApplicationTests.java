e2d3c03f8fa255429b92a3ec1bbf49d73fa87f76
Ashfaq <105435085+Ashfaqbs@users.noreply.github.com>
2026-03-14 07:18:58 +0530

    feat: Add initial Spring Boot application setup with configuration classes and global exception handling

---
+++ dp-springboot/src/test/java/com/patterns/dp_springboot/DpSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patterns.dp_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
